b639b10709aa11548edb0b505687e6384655424c
Leo <leodelmas31@gmail.com>
2024-09-24 11:26:33 +0200

    exercice 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wild-blog';
+  title = 'Bienvenue sur le wild blog de léo';
 }
