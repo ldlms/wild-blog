9abef8cd3561053831076fac0f4542c9670033e2
Leo <leodelmas31@gmail.com>
2024-09-24 13:21:05 +0200

    thirdQuest

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,40 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+
+
+interface Article{
+  title : string,
+  author: string,
+  content : string,
+  image : string
+  isPublished :boolean,
+  comment: string
+}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [NgIf,FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
+
+
+
 export class ArticleComponent {
 
+  article : Article = {
+    title : `titre de l'article`,
+    author: `john doe`,
+    content:`le contenu de l'article`,
+    image:`https://via.placeholder.com/350x150`,
+    isPublished: false,
+    comment:''
+  }
+
+  togglePublication():void{
+    this.article.isPublished = !this.article.isPublished;
+    console.log(this.article.isPublished);
+  }
 }
