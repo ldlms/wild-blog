7948f3e0047a9050dad8ba38b85137bc9d87145c
Leo <leodelmas31@gmail.com>
2024-09-26 15:49:36 +0200

    exercice4Bis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,52 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./article/article.component";
 import { ExerciceDirectivesComponent } from './exercice-directives/exercice-directives.component';
+import { NgClass, NgStyle } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponent],
+  imports: [RouterOutlet, ArticleComponent, NgClass, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Bienvenue sur le wild blog de léo';
+
+  articles = [
+    {
+      title: 'Angular 16: Les nouveautés',
+      author: 'Alice',
+      content: 'Les nouveautés d\'Angular 16 incluent...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: false,
+      comment: '',
+      likes: 120,
+    },
+    {
+      title: 'Développer une API REST',
+      author: 'Bob',
+      content: 'Développer une API REST nécessite...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 75,
+    },
+    {
+      title: 'Pourquoi TypeScript est essentiel ?',
+      author: 'Charlie',
+      content: 'TypeScript apporte de la robustesse...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: false,
+      comment: '',
+      likes: 200,
+    }
+  ];
+
+  areNonePublished():boolean{
+    return this.articles.filter(article => article.isPublished).length === 0;
+  }
+
+
 }
